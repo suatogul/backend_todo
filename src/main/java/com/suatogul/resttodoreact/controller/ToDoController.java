fd0aefe6be50a0135c44ec7209ae7ddc35ea7384
Suat OĞUL <suatogul@gmail.com>
2022-09-06 22:12:58 +0200

    get post delete is set up and working

--- src/main/java/com/suatogul/resttodoreact/controller/ToDoController.java
+++ src/main/java/com/suatogul/resttodoreact/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.suatogul.resttodoreact.controller;
 import com.suatogul.resttodoreact.todo.IToDoService;
 import com.suatogul.resttodoreact.todo.ToDo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,18 @@ public class ToDoController {
     @Autowired
     private IToDoService todoService;
 
+    @GetMapping(value = "/todo")
+    public String getToDos(){
+        return "working get";
+    }
     @PostMapping(value = "/todo")
     public String addToDo(){
     return "working";
+    }
+
+    @DeleteMapping(value = "/todo")
+    public String deleteToDo(){
+        return "working delete";
     }
 
 }
