4a83db3d4d03f68d618c265733fd1ad880724b2e
Suat OĞUL <suatogul@gmail.com>
2022-09-27 22:03:01 +0200

    totally different from the first file

--- src/main/java/com/suatogul/resttodoreact/controller/ToDoController.java
+++ src/main/java/com/suatogul/resttodoreact/controller/ToDoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class ToDoController {
